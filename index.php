215b0a62af7090f576fafd2c9109d0a0a27028bc
Design Tech TI <mrc_490@hotmail.com>
2020-07-29 15:09:09 -0300

    Adicionado o Index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+	<html lang="PT-br">
+		<head>
+			<title>NEBULIMED | Assistência Técnica Autorizada</title>
+			<link rel="stylesheet" type="text/css" href="assets/estilos.css" media="all" />
+			<link rel="shortcut icon" href="images/favicon.png">
+			<meta charset="utf-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+		</head>
+		<body>
+			<header>
+				<img src="images/logotipo.png">
+				<nav>
+					<a href="#">INÍCIO</a>
+					<a href="#">SOBRE</a>
+					<a href="#">AUTORIZADAS</a>
+					<a href="#">CONTATO</a>
+				</nav>		
+			</header>	
+		</body>
+	</html>
