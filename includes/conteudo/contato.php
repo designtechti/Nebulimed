26b90685f882d404860bb8a7632582fbb3c170e1
Design Tech TI <mrc_490@hotmail.com>
2020-08-31 14:51:04 -0300

    Adicionada a Página de Contato

---
+++ includes/conteudo/contato.php
@@ -0,0 +1,17 @@
+<div class="home_banner">
+	<h1>Contato</h1>
+</div>
+<div class="fundo">
+	<div class="transparente">
+		<div id="contato"><img src="images/logotipo.png" width="50%"><br><br></div>
+		<div class="contato">
+			<img src="images/icons/email.png" width="10%"><a href="mailto:assistencia@nebulimed.com.br">assistencia@nebulimed.com.br</a><br><br>
+				<img src="images/icons/phone.png" width="10%">(51) 3212-2748<br><br>
+			<img src="images/icons/address.png" width="10%">Rua Dr. Flores, 105 - Sala 513,<br> Centro Histórico - POA, RS CEP 90020-122<br><br>
+			<img src="images/icons/clock.png" width="10%">Horário de Atendimento: 09hs às 18hs<br><br>			
+		</div>
+		<div class="map">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13817.21850093487!2d-51.2249635!3d-30.0281173!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd8ec788d048d09c6!2sNEBULIMED!5e0!3m2!1spt-BR!2sbr!4v1596049162384!5m2!1spt-BR!2sbr" width="35%" height="250px" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+		</div>	
+	</div>
+</div>
