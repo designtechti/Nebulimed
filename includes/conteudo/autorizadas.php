89dc15ee81f8594d56667109a24840d78228b7f1
Design Tech TI <mrc_490@hotmail.com>
2020-08-31 14:54:51 -0300

    Adicionada a marca Medicate
    Na página Autorizadas

--- includes/conteudo/autorizadas.php
+++ includes/conteudo/autorizadas.php
@@ -14,11 +14,11 @@
 			<a href="emondial.com.br"><img src="images/autorizadas/mondial.png" title="Mondial Eletrodomésticos" alt="Mondial Eletrodomésticos" class="autorizadas" width="20%"></a>
 			<a href="loja.nevoni.com.br"><img src="images/autorizadas/nevoni.png" title="Nevoni" alt="Nevoni" class="autorizadas" width="20%"></a>
 			<a href="nksloja.nksonline.com.br"><img src="images/autorizadas/nks.png" title="NKS" alt="NKS" class="autorizadas" width="20%"></a>
-			<a href="omron.com.br"><img src="images/autorizadas/ns.png" title="NS" alt="NS" class="autorizadas" width="20%"></a>
 			<a href="polarequipamentos.com.br"><img src="images/autorizadas/polar.png" title="Polar Filtros e Bebedouros" alt="Polar Filtros e Bebedouros" class="autorizadas" width="20%"></a>
 			<a href="omron.com.br"><img src="images/autorizadas/two-dogs.png" title="Two Dogs" alt="Two Dogs" class="autorizadas" width="20%"></a>
 			<a href="bellizcompany.com.br"><img src="images/autorizadas/vertix.png" title="Vertix" alt="Vertix" class="autorizadas" width="20%"></a>
 			<a href="vicinibrasil.com.br"><img src="images/autorizadas/vicini.png" title="Vicini" alt="Vicini" class="autorizadas" width="20%"></a>
+			<a href="http://dorja.com.br/marcas/medicate/"><img src="images/autorizadas/medicate.png" title="Medicate" alt="Medicate" class="autorizadas" width="20%"></a>
 		</div>	
 	</div>
 </div>
